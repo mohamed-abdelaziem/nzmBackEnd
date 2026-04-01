a5405dce7c140ed2fdd92a3145af68a36d912bae
= <moabdelaziem@gmail.com>
2026-04-01 10:58:53 +0200

    updated app file

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: env.clientUrl,
-    credentials : true
+     origin: "http://localhost:3000", // عنوان الفرونت المحلي
+  methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
